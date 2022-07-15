17d0e63f7c648bc782c0f9c24ed8bc9f3fef1ab6
Alvin Joshua <84293919+aljoshu@users.noreply.github.com>
2022-07-15 02:19:48 +0000

    v1.0.2 = added hololive JP and EN waifu picker

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord   # import discord.py
 import os        # gae ngambil barang dr environment
-import random
+import random    # random nums gennerator
 
 # Variables/ Properties
 client = discord.Client()
 diceNums = [1, 2, 3, 4, 5, 6]
+hololiveMembers = ['Tokino Sora', 'Roboco', 'Sakura Miko', 'Hoshimachi Suisei', 'AZKi', 'Yozora Mel', 'Shirakami Fubuki', 'Natsuiro Matsuri', 'Akai Haato', 'Aki Rosenthal', 'Minato Aqua', 'Murasaki Shion', 'Nakiri Ayame', 'Yuzuki Choco', 'Oozora Subaru', 'Ookami Mio', 'Nekomata Okayu', 'Inugami Korone', 'Usada Pekora', 'Shiranui Flare', 'Shirogane Noel', 'Houshou Marine', 'Uruha Rushia', 'Amane Kanata', 'Tsunomaki Watame', 'Tokoyami Towa', 'Himemori Luna', 'Kiryu Coco', 'Yukihana Lamy', 'Momosuzu Nene', 'Shishiro Botan', 'Omaru Polka', 'Mano Aloe', 'La+ Darkness', 'Takane Lui', 'Hakui Koyori', 'Sakamata Chloe', 'Kazama Iroha', 'Mori Calliope', 'Takanashi Kiara', "Ninomae Ina'nis", 'Gawr Gura', 'Watson Amelia', 'IRyS', 'Tsukumo Sana', 'Ceres Fauna', 'Ouro Kronii', 'Nanashi Mumei', 'Hakoz Baelz']
 
 
 #lek bot e tangi
@@ -25,10 +26,19 @@ async def on_message(message):
   if message.content.startswith('$aqua'):
     await message.channel.send('https://imgur.com/Kj7QIfl')
 
+  # roll custom num range
+  # if(message.content.startswith('roll') && ):
+  
   # roll a dice
   if message.content.startswith('$roll'):
     chosenNum = random.choice(diceNums)
     await message.channel.send('You rolled> ' + str(chosenNum))
+
+  # random waifu picker
+  lowerCaseMsg = message.content.lower()
+  if(lowerCaseMsg.startswith('$hololiveWaifu') or lowerCaseMsg.startswith('$hw')):
+    chosenHololiveWaifu = random.randrange(0, len(hololiveMembers), 1)
+    await message.channel.send('Your waifu is ' + hololiveMembers[chosenHololiveWaifu])
 
 client.run(os.getenv('TOKEN'))
 
